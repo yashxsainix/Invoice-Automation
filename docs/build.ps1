002aaa2e020f606f435e45a37c826a285f7777eb
Yash Saini <yashsaini30668@gmail.com>
2026-03-17 14:15:49 -0400

    Add files via upload

---
+++ docs/build.ps1
@@ -0,0 +1,30 @@
+#!/usr/bin/env pwsh
+
+$ErrorActionPreference = "Stop"
+Set-StrictMode -Version 2.0
+
+$foldersToMoveToDocs = "articles","contributing","models","scenario-samples","scripts","site-templates"
+foreach($folder in $foldersToMoveToDocs)
+{
+  #Write-Output
+  New-Item -Path ./main/docs -Name $folder -ItemType Directory -Force
+
+  Copy-Item -Force ./main/$folder/* -Destination ./main/docs/$folder -Recurse
+
+}
+
+#docfx metadata ./main/docs/docfx.json --warningsAsErrors $args
+docfx build ./main/docs/docfx.json --warningsAsErrors $args
+# Copy the created site to the pnpcoredocs folder (= clone of the gh-pages branch)
+
+
+Remove-Item ./gh-pages/articles/* -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item ./gh-pages/contributing/* -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item ./gh-pages/models/* -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item ./gh-pages/scenario-samples/* -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item ./gh-pages/scripts/* -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item ./gh-pages/site-templates/* -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item ./gh-pages/docs/* -Recurse -Force -ErrorAction SilentlyContinue
+
+copy-item -Force -Recurse ./main/docs/_site/* -Destination ./gh-pages
+
